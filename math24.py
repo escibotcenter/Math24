9407ac355f1338e67e530caab79c9255c17414ef
PASUNX <34183912+PASUNX@users.noreply.github.com>
2019-08-16 21:34:29 +0700

    Update math24.py

--- math24.py
+++ math24.py
@@ -13,9 +13,12 @@ math_execute = ["+", "-", "*", "/"]
 
 class Math24(object):
     result = None
+    attempts = 0
     def __init__(self, values, timeout=1, debug=False):
         timeout = time.time() + timeout
+        values = [i for i in values if i != " "]
         while time.time() < timeout and self.result == None:
+            self.attempts += 1
             try:
                 value = list(values)
                 execute = random.choice(math_algorithm) % (value.pop(value.index(random.choice(value))), random.choice(math_execute), value.pop(value.index(random.choice(value))), random.choice(math_execute), value.pop(value.index(random.choice(value))), random.choice(math_execute), value.pop(value.index(random.choice(value))))
@@ -25,9 +28,9 @@ class Math24(object):
                 pass
 
     def __repr__(self):
-        return str(self.result)
+        return "%s(%s)" % (self.__class__.__name__, ", ".join("%s=%r" % (key, value) for key, value in self.__dict__.items()))
 
 if __name__ == "__main__":
     while True:
         _input = input("Input: ")
-        print("Solution: %s\n" % (Math24(list(_input) if " " in _input else None if not _input.isdigit() else int(_input)).result))
+        print("Solution: %s\n" % (Math24(list(_input) if " " in _input else None if not _input.isdigit() else int(_input))))
