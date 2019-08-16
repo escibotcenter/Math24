c81ee0733ecc513592ad2fa938aec8f42cd7466c
PASUNX <34183912+PASUNX@users.noreply.github.com>
2019-08-16 19:59:24 +0700

    Update math24.py

--- math24.py
+++ math24.py
@@ -15,17 +15,16 @@ class ThinkValue(object):
     result = None
 
     def __init__(self, values, max_values=4, max_value=50, timeout=1):
-        if max_values in [0, 1]:
-            self.result = values
-        elif len(str(values)) == max_values and all(i.isdigit() for i in list(str(values))):
+        if len(str(values)) == max_values and all(i.isdigit() for i in list(str(values))):
             self.result = list(str(values))
         timeout = time.time() + 1
         while time.time() < timeout and self.result == None:
             v = list(str(values))
             if all(i.isdigit() for i in v):
                 for i in range(len(v) - max_values):
-                    v[random.randint(0, len(v) - 1)] = v[random.randint(0, len(v) - 1)] + v.pop(random.randint(0, len(v) - 1))
-                if len(v) == max_values:
+                    e = random.randint(0, len(v) - 1)
+                    v[e] = v[random.randint(0, len(v) - 1)] + v.pop(random.randint(0, len(v) - 1))
+                if len(v) == max_values and not any(int(i) > int(max_value) for i in v):
                     self.result = v
             else:
                 break
@@ -36,7 +35,7 @@ class Math24(object):
     def __init__(self, values, timeout=1, debug=False):
         if (len([str(i) for i in values if i != " "]) != 4 if isinstance(values, list) else True):
             print("Warning : values should be List, simple %s([4, 4, 4, 4])" % (self.__class__.__name__))
-            dim_values = ThinkValue("".join([str(i) for i in values if i != " "]) if isinstance(values, list) else values, 4, 4)
+            dim_values = ThinkValue("".join([str(i) for i in values if i != " "]) if isinstance(values, list) else values, 4, 50)
             print("%s : returned %s" % (dim_values.__class__.__name__, dim_values.result))
             values = dim_values.result
         timeout = time.time() + timeout
