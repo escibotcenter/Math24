ef839f796237945da5ba28ff9eaa2148be8c7f9c
PASUNX <34183912+PASUNX@users.noreply.github.com>
2019-08-16 21:30:30 +0700

    Update math24.py

--- math24.py
+++ math24.py
@@ -11,41 +11,10 @@ math_algorithm = [
 
 math_execute = ["+", "-", "*", "/"]
 
-class ThinkValue(object):
-    result = None
-
-    def __init__(self, values, max_values=4, max_value=50, timeout=1):
-        if len(str(values)) == max_values and all(i.isdigit() for i in list(str(values))):
-            self.result = list(str(values))
-        timeout = time.time()
-        if len(str(values)) >= max_values:
-            timeout += 1
-        while time.time() < timeout and self.result == None:
-            try:
-                v = list(str(values))
-                if all(i.isdigit() for i in v):
-                    for i in range(len(v) - max_values):
-                        e = random.randint(0, len(v) - 1)
-                        v[e] = int(str(v[e]) + str(v.pop(random.randint(0, len(v) - 1))))
-                    if len(v) == max_values and not any(int(i) > int(max_value) for i in v) and len(str(values)) == len("".join([str(i) for i in v])):
-                        self.result = v
-                else:
-                    break
-            except (IndexError):
-                pass
-
 class Math24(object):
     result = None
-
     def __init__(self, values, timeout=1, debug=False):
         timeout = time.time() + timeout
-        if (len([str(i) for i in values if i != " "]) != 4 if isinstance(values, list) else True):
-            print("Warning : values should be List, simple %s([4, 4, 4, 4])" % (self.__class__.__name__))
-            dim_values = ThinkValue("".join([str(i) for i in values if i != " "]) if isinstance(values, list) else values, 4, 50)
-            print("%s : returned %s" % (dim_values.__class__.__name__, dim_values.result))
-            values = dim_values.result
-            if values == None:
-                timeout -= 1
         while time.time() < timeout and self.result == None:
             try:
                 value = list(values)
